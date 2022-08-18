eb24d69f51d7cb0759547fbcbe22801b5d689b5f
Parth Kathuria <88901443+ParthDevTs@users.noreply.github.com>
2022-08-18 19:58:29 +0530

    added Projects

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+// projects = [
+//   {
+//     name: "markOne",
+//     github: "https://github.com/ParthDevTs/MarkOne",
+//     live: "https://replit.com/@ParthKathuria2/MarkOne?v=1",
+//   },
+// ];
 
+// projects.forEach((item, index) => {
+
+// });
